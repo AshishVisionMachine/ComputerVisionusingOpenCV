534b2af9710d56218355bb834c8076c319640d02
AshishVisionMachine <84072143+AshishVisionMachine@users.noreply.github.com>
2021-06-03 14:23:04 +0530

    sobel

--- main_mask.py
+++ main_mask.py
@@ -9,6 +9,14 @@ imagetitle=['ïnput','output']
 #input_with_filte="filter2D.jpg"
 filer_size_bl=7
 
+sobel_param={
+    "scale": "1",
+    "depth": "3",
+    "delta": "0",
+    "bordertype": "cv.BORDER_DEFAULT"
+
+}
+
 
 if __name__ == "__main__":
     
@@ -25,9 +33,9 @@ if __name__ == "__main__":
     #img_re=np.reshape(imsat,(height_i,width_i,ch_i))
     
     img_re=image_mask.bilateral_filter(im,filer_size_bl)
-    img_re=image_mask.image_border(img_re)
-    
-    imagelist.append(img_re)
+    #img_re=image_mask.image_border(img_re)
+    im=image_mask.sobelfilter(img_re,sobel_param)
+    imagelist.append(im)
     
     dis_obj.Display_plot(imagetitle,imagelist,len(imagetitle))
     
--- mask.py
+++ mask.py
@@ -66,4 +66,17 @@ class Imagemask:
         
         
         return dst
-        
+    
+    def sobelfilter(self,image,config) :
+    
+        img = cv.GaussianBlur(image, (config.depth, config.depth), 0)    
+        img = cv2.cvtColor(img, cv.COLOR_BGR2GRAY)
+        #cv2.Sobel(image, ddepth, 1, 0, ksize=3, scale=config.scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+        grad_x = cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+
+        grad_y = cv2.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+        abs_grad_x = cv2.convertScaleAbs(grad_x)
+        abs_grad_y = cv2.convertScaleAbs(grad_y)
+        grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    
+
